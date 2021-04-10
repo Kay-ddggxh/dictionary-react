ea3af47f03e61f74783312da018b496827e25c01
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2021-04-10 18:03:02 +0100

    changed styling a touch

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,19 +5,17 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <section>
-        {" "}
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function (definition, index) {
-          return (
-            <div key={index}>
-              <div className="definition">{definition.definition}</div>
-              <div className="example">Example: "{definition.example}"</div>
-              <Synonyms synonyms={definition.synonyms} />
-            </div>
-          );
-        })}
-      </section>
+      {" "}
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">Example: "{definition.example}"</div>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
   );
 }
